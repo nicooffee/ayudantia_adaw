22a3b74de9d61eb136c4dd31f7e1bd31815b6808
Nicolas Honorato <nicolas.honorato@usach.cl>
2021-05-26 17:39:11 -0400

    Form corregido y envío de información a la hoja html

--- 1_clase/demo.py
+++ 1_clase/demo.py
@@ -1,7 +1,8 @@
 #$env:FLASK_APP = "demo.py"
 from flask import (
     Flask,
-    render_template
+    render_template,
+    request
 )
 from datetime import datetime
 import json
@@ -21,6 +22,25 @@ def funcion_palabra():
     f = open('palabra_data.json',)
     lista = json.load(f)
     return render_template('palabra.html',lista_palabra=lista)
+
+@app.route("/form-ejemplo", methods=['GET','POST'])
+def funcion_form():
+    form = request.form
+    if request.method == "POST":
+        id = form.get('id') #'id' es el atributo name de los input
+        tipo = form.get('tipo')
+        palabra = form.get('palabra')
+        def_eng = form.get('def_eng')
+        #envio la informacion recibida desde el form y la envio a la misma hoja html
+        return render_template('ejemplo-form.html',
+            post=True,
+            id = id, 
+            tipo = tipo, 
+            palabra = palabra, 
+            def_eng = def_eng)
+    print("get")
+    #Si la solicitud es tipo get llego a esta instruccion
+    return render_template('ejemplo-form.html',post=False)
 
 if __name__=="__main__":
     with app.app_context():
